179d8397ea7afe2f406cc336fa8320eee3eaebcf
alexanderkif <alexander_kif@mail.ru>
2017-10-06 15:32:26 +0400

    222

--- src/main/java/ga/skif/sprsec/controllers/IndexController.java
+++ src/main/java/ga/skif/sprsec/controllers/IndexController.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Objects;
 
-//@Controller
+@Controller
 @SessionAttributes(value = "userJSP")
 public class IndexController {
 
